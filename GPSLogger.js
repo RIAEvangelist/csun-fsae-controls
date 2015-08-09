d415a3ec162e7b9ae14e110b20eb8442211ed54d
MaybeRex <marioxcore1@gmail.com>
2015-08-08 20:29:24 -0700

    Orgianizing and structuring everything

--- GPSLogger.js
+++ GPSLogger.js
@@ -1,55 +1,79 @@
+//Libraries
 var phidget = require('phidgetapi').phidget;
 var fs = require('fs');
 
+//Global Variables
 var GPS=new phidget();
 
-GPS.on(
-    'log',
-    function(data){
-        console.log('log ',data);
-    }
-);
+//Creating headers
+var GPSData={
+    header:['time','lat','lon','alt','heading','velocity'],
+    data:{}
+};
 
-GPS.on(
-    'error',
-    function(data){
-        console.log('error ',data);
-    }
-);
-
-GPS.on(
-    'changed',
-    function(data){
-        console.log('changed');
-        console.log('data ',data);
-    }
-);
-
-GPS.on(
-    'phidgetReady',
-    function(){
-        console.log('GPS ready');
-        console.log(GPS.data);
-
-    }
-);
-
-
-/*
- * Connect to phidget
- */
+//Connecting GPS
 GPS.connect(
     {
-        type:'PhidgetGPS'
+        type    :'PhidgetGPS'
     }
 );
 
-/*
- * an example of how to see the data being transferred to and from the phidget
- *
- GPS.connect(
-      {
-          rawLog:true
-       }
-  );
-*/
+//On Calls
+GPS.on(
+    'phidgetReady',
+    GPSReady
+);
+
+GPS.on(
+    "error",
+    errorHandler
+);
+
+//Initialization of GPS
+function GPSReady() {
+    GPS.on(
+        'changed',
+        updateGPS
+    );
+    setInterval(
+        logGPS,
+        1000
+    );
+}
+
+function updateGPS(data){
+    console.log('look here!!');
+
+    switch(data.key){
+        case 'Position':
+            var location=data.Position.split('/');
+            data.Position={
+                lat:Number(location[0]),
+                lon:Number(location[1]),
+                alt:Number(location[2])
+            }
+            break;
+        case 'Velocity' :
+            data.Velocity=Number(data.Velocity);
+            break;
+        case 'Heading' :
+            data.Heading=Number(data.Heading);
+            break;
+    };
+}
+
+
+function logGPS(data) {
+
+    fs.appendFile('logs/GPSData.txt', 'lol hello :)', function (err) {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    }
+                 );
+
+}
+
+//Error Handler
+function errorHandler(data){
+    console.log('Somethings not right... ', data);
+}
