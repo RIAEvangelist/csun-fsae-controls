a8997f53aa28c77c578909c8614531da89031a20
MaybeRex <marioxcore1@gmail.com>
2015-08-14 15:33:07 -0700

    cleaned the code and got rud of unnecassary lones

--- SensorLogger.js
+++ SensorLogger.js
@@ -4,8 +4,6 @@ var util = require('util');
 var IK = new Phidget;
 
 IK.observeOutputs(outputs);
-IK.observeInputs(inputs);
-IK.observeSensors(sensors);
 IK.observeRawSensors(rawSensors);
 
 IK.phidget.on(
@@ -16,17 +14,7 @@ IK.phidget.on(
 IK.phidget.connect();
 
 function init(){
-
-
-}
-
-
-function sensors(changes){
-    for(var i in changes){
-        var change=changes[i];
-        //see specific info about each change
-        //console.log(change);
-    }
+    console.log("Head Pressure Test Program");
 }
 
 
@@ -36,15 +24,11 @@ function rawSensors(changes){
     var area = 0.836; //in^2
     for(var i in changes){
         var change=changes[i];
-        //see specific info about each change
-        //console.log(change);
     }
         //lbs =  (0.13 * Math.pow( 2.718282, (0.0009 * IK.rawSensors[0]))  );  //Exponential Curve fit, f(x) > 0.1 lbs
-        lbs =  ((0.00000077912 * (IK.rawSensors[0] * IK.rawSensors[0] )) - (0.00247485588 * IK.rawSensors[0]) + 2.52329520725);  // Poly Curve fit
+        lbs =  ((0.00000077912 * (IK.rawSensors[0] * IK.rawSensors[0] )) - (0.00247485588 * IK.rawSensors[0]) + 2.52329520725);  // Poly                                                                                                                            //Curve fit
                                                                                                             // 0.5lbs < f(x) < 1.7 lbs
-        //see updated IK data after all change
         console.log(lbs);
-
 }
 
 function outputs(changes){
@@ -52,16 +36,5 @@ function outputs(changes){
         var change=changes[i];
     }
 
-}
-
-function inputs(changes){
-    for(var i in changes){
-        var change=changes[i];
-        //see specific info about each change
-        //console.log(change);
-    }
-
-    //see updated IK data after all changes
-    //console.log('Inputs',IK.inputs);
 }
 
