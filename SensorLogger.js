7e196835fa07d4b2243c15d74ebc24d3d2b9c38c
MaybeRex <marioxcore1@gmail.com>
2015-08-14 10:43:09 -0700

    fixed a problem with a ^2 I was having

--- SensorLogger.js
+++ SensorLogger.js
@@ -16,6 +16,8 @@ IK.phidget.on(
 IK.phidget.connect();
 
 function init(){
+
+
 }
 
 
@@ -26,7 +28,7 @@ function sensors(changes){
         //console.log(change);
     }
 }
-}
+
 
 function rawSensors(changes){
 
@@ -38,7 +40,7 @@ function rawSensors(changes){
         //console.log(change);
     }
         //lbs =  (0.13 * Math.pow( 2.718282, (0.0009 * IK.rawSensors[0]))  );  Exponential Curve fit, f(x) > 0.1 lbs
-        lbs = (0.00000077912 * (IK.rawSensors[0])^2) - (0.00247485588 * IK.rawSensors[0]) + 2.52329520725;  // Poly Curve fit
+        lbs =  ((0.00000077912 * (IK.rawSensors[0] * IK.rawSensors[0] )) - (0.00247485588 * IK.rawSensors[0]) + 2.52329520725);  // Poly Curve fit
                                                                                                             // 0.5lbs < f(x) < 1.7 lbs
         //see updated IK data after all change
         console.log(lbs);
@@ -53,4 +55,13 @@ function outputs(changes){
 }
 
 function inputs(changes){
+    for(var i in changes){
+        var change=changes[i];
+        //see specific info about each change
+        //console.log(change);
+    }
+
+    //see updated IK data after all changes
+    console.log('Inputs',IK.inputs);
 }
+
