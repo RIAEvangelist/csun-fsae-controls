6bac078e73c41609adddd45d8d17896a2ff4bc88
MaybeRex <marioxcore1@gmail.com>
2015-08-22 11:43:18 -0700

    cleaned up code, removed unnecassary bits

--- SensorLogger.js
+++ SensorLogger.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var util = require('util');
 var IK = new Phidget;
 
-IK.observeOutputs(outputs);
 IK.observeRawSensors(rawSensors);
 
 IK.phidget.on(
@@ -28,13 +27,5 @@ function rawSensors(changes){
         //lbs =  (0.13 * Math.pow( 2.718282, (0.0009 * IK.rawSensors[0]))  );  //Exponential Curve fit, f(x) > 0.1 lbs
         lbs =  ((0.00000077912 * (IK.rawSensors[0] * IK.rawSensors[0] )) - (0.00247485588 * IK.rawSensors[0]) + 2.52329520725);  // Poly                                                                                                                            //Curve fit
                                                                                                             // 0.5lbs < f(x) < 1.7 lbs
-        console.log(lbs);
+        console.log((Math.round(lbs*10000))/10000);
 }
-
-function outputs(changes){
-    for(var i in changes){
-        var change=changes[i];
-    }
-
-}
-
